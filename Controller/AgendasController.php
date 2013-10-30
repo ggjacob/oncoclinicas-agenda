877e95b7adf8939aaba64369cf155069da6a8bcb
João Lima <joao.lima@vilivesoft.com>
2013-10-30 14:27:25 -0200

    adjusting times

--- Controller/AgendasController.php
+++ Controller/AgendasController.php
@@ -16,7 +16,7 @@ class AgendasController extends AppController {
 	public $components = array('Paginator');
 	protected $hours = array("00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", 
 			"15", "16", "17", "18", "19", "20", "21", "22", "23");
-	protected $minutes = array("00", "25", "50", "75");
+	protected $minutes = array("00", "15", "30", "45");
 
 /**
  * index method
